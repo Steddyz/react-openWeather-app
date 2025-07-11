578c4311544c0a28dfda4b7ea25f54be0db16809
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-07-11 17:12:53 +0300

    add apiWeather

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { getWeather } from "./api/weather";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <h1 className="weather-title">Погодное приложение</h1>
+        <div className="location-controls">
+          <button>Севастополь</button>
+          <button>Моё местоположение</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/api/weather.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.OPENWEATHER_API_KEY;
+
+export const getWeather = async (lat: number, lon: number) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+};
+
+export const searchCities = async (query: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error searching cities:", error);
+    throw error;
+  }
+};
